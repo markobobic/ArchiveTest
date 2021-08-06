0455ab96e948a7f23491e8faf91fcd98dc7fae23
markobobic <bobic015@gmail.com>
2021-08-06 17:03:55 +0200

    refactor: removed comments and set to run bulkTests also

--- ArchiveData/Program.cs
+++ ArchiveData/Program.cs
@@ -10,43 +10,11 @@ namespace ArchiveData
     {
         static void Main(string[] args)
         {
-            //TEST 1
-            //10 000 000 bulk insert + 10 500 
-            // limit 10 010 000
-            //arhivaranje 2 000 000 
-
-            //TEST 2
-            //10 000 000 bulk insert + 10 500 
-            // limit 10 010 000
-            //arhivaranje 200 000 
-
-            //TEST 3
-            //5 000 000 bulk insert + 10 500 
-            // limit 5 010 000
-            //arhivaranje 1 000 000 
-
-            //TEST 4
-            //5 000 000 bulk insert + 10 500 
-            // limit 5 010 000
-            //arhivaranje 200 000
-
-            //TEST 5
-            //1 000 000 bulk insert + 10 500 
-            // limit 1 010 000
-            //arhivaranje 500 000
-
-            //TEST 6
-            //1 000 000 bulk insert + 10 500 
-            // limit 1 010 000
-            //arhivaranje 200 000
-
-            // testirati prvobitni bulk i SaveChanges()
-            // za svaki test 3 puta pokrenuti i uzeti srednju vrednost 
-            // MySQL i MSSqlServera poredjenje preformansi 
 
             using var db = new ApplicationDBContext();
             TestArchiveService services = new TestArchiveService(new MockDataService());
             services.RunOnlySaveChangesTests(db);
+            services.RunOnlyBulkTests(db);
             //services.Reset(db);
             
            
